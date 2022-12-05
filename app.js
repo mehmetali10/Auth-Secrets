269dd3b36277ab2de76e47cc036863f5699efe54
mehmetali10 <mhmtali2316@gmail.com>
2022-12-05 03:37:43 +0300

    add enviroment variables

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -17,9 +17,8 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secrect = "Thisminesecret";
 
-userSchema.plugin(encrypt, {secret: secrect, encryptedFields: ["password"]});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
